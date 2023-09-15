fb802253da705c868fa592707f5188dc7e980287
maximsmol <maximsmol@gmail.com>
2023-09-15 11:35:20 -0700

    fixies
    Signed-off-by: maximsmol <maximsmol@gmail.com>

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -9,18 +9,74 @@
 )
 (create_index
   "create" @keyword
+  "unique"? @keyword
   "index" @keyword
   "on" @keyword
 )
 (create_table
   "create" @keyword
   "table" @keyword
-  name: _ @entity.name.class
+  name: _ @entity.name
 )
 (create_type
   "create" @keyword
   "type" @keyword
-  name: _ @entity.name.type
+  name: _ @entity.name
   "as" @keyword
   "enum" @keyword
+)
+(create_schema
+  "create" @keyword
+  "schema" @keyword
+  (
+    "if" @keyword
+    "not" @keyword
+    "exists" @keyword
+  )?
+  (identifier_single) @entity.name
+)
+(create_extension
+  "create" @keyword
+  "extension" @keyword
+  (
+    "if" @keyword
+    "not" @keyword
+    "exists" @keyword
+  )?
+  (name) @entity.name
+)
+(create_sequence
+  "create" @keyword
+  "sequence" @keyword
+  (
+    "if" @keyword
+    "not" @keyword
+    "exists" @keyword
+  )?
+  (identifier) @entity.name
+)
+(do
+  "do" @keyword
+)
+(meta_command
+  "\\" @keyword
+  command: _ @keyword
+)
+(set
+  "set" @keyword
+  (name) @entity.name
+  "=" @keyword
+  ["on" "off"] @constant.language
+)
+(drop
+  "drop" @keyword
+  ["schema" "role" "table" "trigger"] @keyword
+  "if" @keyword
+  "exists" @keyword
+  (identifier) @entity.name
+  (
+    "on" @keyword
+    (identifier) @entity.name
+  )?
+  "cascade"? @keyword
 )
